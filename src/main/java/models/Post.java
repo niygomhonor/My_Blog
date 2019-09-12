431b5b1b42f50ace04416300b5d54edbf64a230a
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-09-12 15:19:51 +0200

    Add more methods

--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -2,23 +2,20 @@ package models;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-//public class Post {
-//
-//    public Post (String content){
-//
-//    }
-//}
+
 public class Post {
     private String content;
     private static ArrayList<Post> instances = new ArrayList<>(); // I’m new. When do I get created?
     private boolean published; //i’m new
     private LocalDateTime createdAt; //see constructor and my method
-
+    private int id;
 
     public Post(String content) {
         this.content = content;
+        this.published = false;
+        this.createdAt = LocalDateTime.now();
         instances.add(this); //Also new. Can you figure out what I do and how I work?
-
+        this.id = instances.size();
     }
     public String getContent() {
         return content;
@@ -37,5 +34,10 @@ public class Post {
     }
     public LocalDateTime getCreatedAt() {
         return createdAt;
+    }public int getId() {
+        return id;
+    }
+    public static Post findById(int id){
+        return instances.get(id-1); //why minus 1? See if you can figure it out.
     }
 }
